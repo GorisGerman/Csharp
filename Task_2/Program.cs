4886bb48b74ad0d499d8c4d0e6215e1605d11fd3
Goris German <mefisto404@gmail.com>
2022-07-08 23:54:18 +0300

    task 2 fixed

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -4,17 +4,10 @@ int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
 if (numberA > numberB)
-{
-   Console.WriteLine("Первое число больше второго");
-   Console.WriteLine(numberA);
-}
+    Console.WriteLine($"Число {numberA} больше числа {numberB}");
 else if (numberA < numberB)  
-{
-    Console.WriteLine("Второе число больше первого");
-    Console.WriteLine(numberB);  
-}
- else
- {
+    Console.WriteLine($"Число {numberB} больше числа {numberA}");
+else
     Console.WriteLine("Числа равны");
- }
+
     
