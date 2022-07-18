c3f755c595918ae5427183457e9d5c31b734af11
Goris German <mefisto404@gmail.com>
2022-07-18 20:45:44 +0300

    DZ completed

---
+++ Task_19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+string strA = Console.ReadLine();
+int Length = 5;
+if(strA.Length > Length || strA.Length < Length)
+{
+    Console.WriteLine($"Число {strA} - не пятизначное");
+}
+else
+{
+    char[] arr = strA.ToCharArray();
+    Array.Reverse(arr);
+    string strB = new String(arr);
+    if (strA == strB) 
+    {
+        Console.WriteLine($"{strA} - палиндром");
+    }
+    else 
+    {
+        Console.WriteLine($"{strA} - Не палиндром");
+    }
+}
---
+++ Task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int I = 1;
+while (I < N + 1)
+{
+    Console.WriteLine(Math.Pow(I, 3));
+    I++;
+}
