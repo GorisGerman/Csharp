91a115e42ab1ecc67840c50670ac62c0572be8e9
Goris German <mefisto404@gmail.com>
2022-08-04 15:59:06 +0300

    DZ added

---
+++ Task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число А: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+for (int i = 0; numA != 0; i++)
+{
+    sum += numA % 10;
+    numA /= 10;
+}
+Console.WriteLine(sum);
---
+++ Task_29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int length = 8;
+int[] array = new int[length];
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine("Введите значение элемента массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
